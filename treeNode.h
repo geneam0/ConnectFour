dfa31cf4adf70f36d4e9776fc731af25383143b7
genelam26 <lamgene26@gmail.com>
2019-07-09 13:47:43 -0400

    Update treeNode.h

--- treeNode.h
+++ treeNode.h
@@ -1,3 +1,5 @@
+#ifndef __TREENODE__
+#define __TREENODE__
 class treeNode{
   private:
     Item data;
@@ -6,5 +8,9 @@ class treeNode{
     size_t numChildren;
     size_t capChildren;
   public:
-    
+    getChild //get ith children
+    getChild //get all children
+    setChild //ith children to a different state
+    addChild //
 };
+#endif
