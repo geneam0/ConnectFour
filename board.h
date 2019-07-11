826f2c5315110e26f0db388bf8b7a56ac9cd4c91
Gene Lam <lamgene26@gmail.com>
2019-07-11 18:20:52 -0400

    working on board

--- board.h
+++ board.h
@@ -9,22 +9,36 @@ class Board{
 		size_t columns=6;
 		char arr[rows][columns];
 	public:
+		// constructors
 		Board(){
 			for(size_t i=0; i<rows; i++){
-				for(size_t j=0; j<target.columns; j++){
+				for(size_t j=0; j<columns; j++){
 					arr[i][j]="-";
 				}
 			}
 		}
-	void printBoard(const Board target) const {
-		for(size_t i=0; i<target.rows; i++){
-			for(size_t j=0; j<target.columns; j++){
-				cout<<target[i][j]<<"\t";
+		Board(Board target){
+			for(size_t i=0; i<target.rows; i++){
+				for(size_t j=0; j<target.columns; j++){
+					arr[i][j]=target.arr[i][j];
+				}
 			}
-			cout<<"\n";
 		}
-	}
+		Board(char* a){
+			for(size_t i=0; i<r; i++){
+				for(size_t j=0; j<c; j++){
+					arr[i][j]=a[i][j];
+				}
+			}
+		}
 };
-
+void printBoard(const Board target) const {
+	for(size_t i=0; i<target.rows; i++){
+		for(size_t j=0; j<target.columns; j++){
+			cout<<target[i][j]<<"\t";
+		}
+		cout<<"\n";
+	}
+}
 
 #endif
