5d0efd72d920f3117adc6600ad8f92ffc1dfd0aa
Gene Lam <lamgene26@gmail.com>
2019-07-11 18:15:50 -0400

    working on board

--- board.h
+++ board.h
@@ -1,7 +1,30 @@
 #ifndef _BOARD_H_
 #define _BOARD_H_
+#include <iostream>
+using namespace std::cout;
 
 class Board{
-
+	private:
+		size_t rows=7;
+		size_t columns=6;
+		char arr[rows][columns];
+	public:
+		Board(){
+			for(size_t i=0; i<rows; i++){
+				for(size_t j=0; j<target.columns; j++){
+					arr[i][j]="-";
+				}
+			}
+		}
+	void printBoard(const Board target) const {
+		for(size_t i=0; i<target.rows; i++){
+			for(size_t j=0; j<target.columns; j++){
+				cout<<target[i][j]<<"\t";
+			}
+			cout<<"\n";
+		}
+	}
 };
+
+
 #endif
