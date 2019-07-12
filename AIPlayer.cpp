7f2bb9c129245790dd450738fe9c14c22726a1c9
genelam26 <lamgene26@gmail.com>
2019-07-12 14:37:58 -0400

    Update AIPlayer.cpp

--- AIPlayer.cpp
+++ AIPlayer.cpp
@@ -1,9 +1,18 @@
 #include "AIPlayer.h"
 
 //ith children to a different state
-void AIPlayer::setChild(size_t i); 
-void AIPlayer::addChild(); //
-Item* AIPlayer::generateChildren();
+void AIPlayer::setChild(size_t i){
+
+}
+
+void AIPlayer::addChild(){
+
+}
+
+Item* AIPlayer::generateChildren(){
+
+}
+
 // main method for the AI to determine which column to place in
 size_t AIPlayer::move(const Board b){
   // return BFS(b);
