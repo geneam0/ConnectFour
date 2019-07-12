f1c5ff20355e574dae64cdd6b03654a81ffc9bb5
meriselleb <48763261+meriselleb@users.noreply.github.com>
2019-07-12 15:15:47 -0400

    Update tempBFS.cpp

--- tempBFS.cpp
+++ tempBFS.cpp
@@ -1,7 +1,28 @@
 #ifndef __BFS_H__
 #define __BFS_H__
 
+#include "main.cpp"
+#include "priorityQueue.cpp"
+
+PriorityQueue<Item> choices;
+PriorityQueue<Item> children;
 
 
+size_t BFS(init) {
+  /*pseudocode:
+  generate children and add to siblings queue.
+  read top queue and increase depth count by 1, 
+  calculate priority and cost
+  cost being priority + 5 - count
+  then add to priority queue
+  then shift over 
+  and gradually fill priority queue, after four levels, top of priority queue is the column returned. 
+  */
+  
+  
 
-#endif
+   
+   
+
+
+#endif  
