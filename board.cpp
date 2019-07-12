f726fc7f51121802dc486379497a928fd920b33c
meriselleb <48763261+meriselleb@users.noreply.github.com>
2019-07-12 12:53:35 -0400

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -9,17 +9,35 @@ bool Board::fullColumn(size_t column) {
   for(int i = 0; i < rows; i++) {
     if(arr[i][column] == "-") {
       return false;
-    } else {
-      return true;
-    }
+    } 
+  }
+  return true;
 }
 
 bool Board::boardFull() {
+  for(int i = 0; i < rows; i++) {
+    for(int j = 0; j < columns; j++) {
+      if (ar[i][j] == "-") {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
-bool Board::spaceEmpty() {
+bool Board::spaceEmpty(size_t row, size_t column) {
+  if(arr[row][column] == "-") {
+     return false;
+  } else {
+    return true;
+  }    
 }
 
 void Board::addPiece(char entry, size_t column) {
- 
+  if(fullColumn) { /* invalid message and reprompt */ return; }
+  if(boardFull) { return; }
+  while(!spaceEmpty(i, column)) {
+    i++;
+  }
+  board[i][column] = entry;
 }
