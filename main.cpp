56762b46fa767db0cfd5281f55d07e20f564ffe2
genelam26 <lamgene26@gmail.com>
2019-07-12 13:26:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include "board.h"
+#include "AIPlayer.h"
 #include <iostream>
 using namespace std::cin;
 using namespace std::cout;
 
 int main(){
-	Board init; 
+	Board init=new Board(); 
+	AIPlayer ai=new AIPlayer();
+	
 	// playerInput is the column the player chooses to put a chip into
 	size_t playerInput;
-	while(!init.hasWinner()){
+	
+	while(!init.hasWinner() && ){
 		cin>>playerInput;
 		init.updateBoard(playerInput);
 		cout<<init;
 	}
 	return 0;
 }
-
-// write hasWinner function
-//write updateBoard function
