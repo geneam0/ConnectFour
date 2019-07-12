3306a0ef93d6113204626888a33c45951d9b7325
meriselleb <48763261+meriselleb@users.noreply.github.com>
2019-07-12 15:18:10 -0400

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,16 @@
+#ifndef __NODE_H__
+#define __NODE_H__
+
+#include "board.cpp"
+
+class Node {
+private: 
+  size_t column;
+  size_t cost;
+  Board possibleBoard;
+  
+public:
+  void calculateCost();
+  
+
+};
