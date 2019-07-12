84ce2313e68a403512a2ce43a55c9420354d1129
meriselleb <48763261+meriselleb@users.noreply.github.com>
2019-07-12 13:23:10 -0400

    Update priorityQueue.h

--- priorityQueue.h
+++ priorityQueue.h
@@ -1,7 +1,38 @@
 #ifndef __PRIORITYQUEUE_H__
 #define __PRIORITYQUEUE_H__
 
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
 
+using namespace std;
+
+template <class Item>
+class PriorityQueue {
+    public:
+        const size_t DEF_CAP = 30;
+
+        PriorityQueue();
+        PriorityQueue(const PriorityQueue<Item>& source);
+        ~PriorityQueue();
+
+        void push(const Item& entry);
+        void pop();
+        void operator=(const PriorityQueue<Item>& source);
+
+        size_t size() const;
+        bool empty() const;
+        Item front() const;
+
+    private:
+        Item* data;
+        size_t first;
+        size_t last;
+        size_t used;
+        size_t capacity;
+        size_t nextIndex(size_t i) const {return (i+1)%capacity;}
+        size_t prevIndex(size_t i) const;
+};
 
 
 
