83c95b12467a687477542809dd593b116866a1f9
genelam26 <lamgene26@gmail.com>
2019-07-12 14:37:18 -0400

    Update AIPlayer.cpp

--- AIPlayer.cpp
+++ AIPlayer.cpp
@@ -1,15 +1,13 @@
 #include "AIPlayer.h"
 
-void getChild(size_t i); //get ith children
-void getChild(); //get all children
-void setChild(size_t i); //ith children to a different state
-void addChild(); //
-void generateChildren //
-
+//ith children to a different state
+void AIPlayer::setChild(size_t i); 
+void AIPlayer::addChild(); //
+Item* AIPlayer::generateChildren();
 // main method for the AI to determine which column to place in
-size_t move(const Board b){
+size_t AIPlayer::move(const Board b){
   // return BFS(b);
   // return DFS(b);
   // return MiniMax(b);
   // return IterativeDeepening(b);
- }
+}
