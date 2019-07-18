e35821101738598818875db0853d091dac85a5f1
genelam26 <lamgene26@gmail.com>
2019-07-18 18:11:35 -0400

    Update tempDFS.cpp

--- tempDFS.cpp
+++ tempDFS.cpp
@@ -1,12 +1,8 @@
 #include "Board.h"
 #include "Node.h"
-#include "PriorityQueue.h"
+#include "Stack.h"
 
 size_t DFS(const Board b){
   Item* root=new Item(b);
-  size_t level=0;
-  while(level<=depth){
-    root.nextChild(0);
-    calculateDFSCost();
-  }
+  
 }
