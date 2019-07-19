97d1b981042fc98b54fd8e063aeab2d1adc97a6e
genelam26 <lamgene26@gmail.com>
2019-07-19 17:27:47 -0400

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -3,5 +3,22 @@
 
 #include "node.h"
 
+Board DFS(const Board b){
+  // Base Case: returns the winning board or tied board if no other solution possible
+  if(b.hasWinner()||(b.boardFull()&&childrenStack.empty())) return b;
+  /*
+   * If the board isn't full, DFS will push in the child nodes into the childrenStack and the parent nodes into the 
+   * parentStack. Then it will perform DFS on the most leftest child node. If the board is full, it will use the 
+   * childrenStack to find the next node to perform DFS on.
+   */
+  else if(!b.boardFull()){
+    parentStack.push(b);
+    //!!!! IMPLEMENT GENERATE CHILDREN
+    childrenStack.push(generateChildren(b));
+  } 
+  Board onTop=childrenStack.top();
+  childrenStack.pop();
+  DFS(onTop);
+}
 
 #endif
