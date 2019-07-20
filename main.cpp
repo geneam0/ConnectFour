ce095cc25ecbf2c954e0c2cb0582795ef050e325
genelam26 <lamgene26@gmail.com>
2019-07-19 23:17:09 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 using namespace std::cin;
 using namespace std::cout;
 
+// for when we want to make a Player vs AI Connect Four Game
+/*
 void makeMove(char Player, Board& b){
 	if(Player==init.getUser()){
 		size_t playerInput;
@@ -16,12 +18,54 @@ void makeMove(char Player, Board& b){
 	}
 	cout<<init;
 }
+*/
 
 int main(){
 	// initialize the board and AI
-	Board init = Board(); 
-	AIPlayer ai = AIPlayer();
+	Board init = new Board(); 
+	Node AI = new Node();
+	// test DFS
+	init.addPiece('X',0); init.addPiece('O',0);
+	init.addPiece('X',0); init.addPiece('O',0);
+	init.addPiece('X',0); init.addPiece('O',0);
+	init.addPiece('X',1); init.addPiece('O',1);
+	init.addPiece('X',1); init.addPiece('O',1);
+	init.addPiece('X',1); init.addPiece('O',1);
+	init.addPiece('X',2); init.addPiece('O',2);
+	init.addPiece('X',2); init.addPiece('O',2);
+	init.addPiece('X',2); 
+	/* Should look like dis:
+	Before               After
+	O O - - - - -        O O O - - - -
+	X X X - - - -        X X X - - - -
+	O O O - - - - _____\ O O O - - - -
+	X X X - - - -      / X X X - - - -
+	O O O - - - -        O O O - - - -
+	X X X - - - -        X X X X - - - (wow, that glo' up)
+	*/
+	cout<<AI.DFS(init);
+	delete init;
 	
+	// initialize the board again
+	Board init = new Board();
+	// test BFS
+	
+	delete init;
+	
+	// initalize the board again
+	Board init = new Board();
+	// test MiniMax
+	
+	delete init;
+	
+	// initalize the board again
+	Board init = new Board();
+	// test Iterative Deepening
+	
+	delete init;
+	
+	// for when we want to make a Player vs AI Connect Four Game
+	/*
 	// playerInput is the column the player chooses to put a chip into
 	while(!init.hasWinner() && !init.boardFull()){
 		makeMove(init.getUser(),init);
@@ -36,5 +80,6 @@ int main(){
 		}
 	}
 	cout<<"You tied!";
+	*/
 	return 0;
 }
