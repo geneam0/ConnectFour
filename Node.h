dd7e1d112b770c1d2f917f1784eea69a9b2fee45
genelam26 <lamgene26@gmail.com>
2019-07-20 13:41:22 -0400

    Update Node.h

--- Node.h
+++ Node.h
@@ -6,44 +6,40 @@
 #include "PriorityQueue.h"
 
 class treeNode {
-public:
+  private: 
+    Board possibleBoard;   // data part of node ("parent" board)
+    treeNode* parent;
+    treeNode *children[7];        // link part of node (array of pointers) 
+    Stack<Board> parentStack;
+    Stack<Board> childrenStack;
+    Queue<treeNode> gameTree;
+    Stack<Board> winningPath;
+  public:
 
-  // constructors
-  treeNode();
-  treeNode(size_t col);
-
-  // get data
-  treeNode getChild(size_t i)                               { return children[i]; }
-  treeNode getChildren()                                    { return children; }
-  Board getBoard()                                   { return possibleBoard; }
-  treeNode* getParent()                                     { return parent; }
+    // constructors
+    treeNode();
+    treeNode(size_t col);
   
+    // get data
+    treeNode getChild(size_t i)                               { return children[i]; }
+    treeNode getChildren()                                    { return children; }
+    Board getBoard()                                   { return possibleBoard; }
+    treeNode* getParent()                                     { return parent; }
+  
+    // add/set data
+    void addChild(treeNode* newChildPtr, size_t i);   
+    void generateChildren(char turn); // given a node, what are the next possible moves
+    void setBoard(Board b)                             { possibleBoard = b; }
+    void setParent(treeNode* p)                               { parent = p; }
 
-  // add/set data
-  void addChild(treeNode* newChildPtr, size_t i);   
-  void generateChildren(char turn); // given a node, what are the next possible moves
-  void setBoard(Board b)                             { possibleBoard = b; }
-  void setParent(treeNode* p)                               { parent = p; }
+    // overloaded operators
+    treeNode operator=(treeNode source);
 
-  // overloaded operators
-  treeNode operator=(treeNode source);
-
-  // search algorithms
-  Board DFS(const Board b, char turn);
-  Board Minimax(const Board b); 
-  void BFS();
-  void ID();
-
-
-private: 
-  Board possibleBoard;   // data part of node ("parent" board)
-  treeNode* parent;
-  treeNode *children[7];        // link part of node (array of pointers) 
-  Stack<Board> parentStack;
-  Stack<Board> childrenStack;
-  Queue<treeNode> gameTree;
-  Stack<Board> winningPath;
+    // search algorithms
+    Board DFS(const Board b, char turn);
+    Board Minimax(const Board b); 
+    void BFS();
+    void ID();
 };
-
 
 #endif
