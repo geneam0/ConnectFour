82cc03bd4e45412809f4f391812b79f144303cdd
genelam26 <lamgene26@gmail.com>
2019-07-21 14:20:30 -0400

    Update tempMiniMax.cpp

--- tempMiniMax.cpp
+++ tempMiniMax.cpp
@@ -1,22 +1,29 @@
 void MiniMax(const treeNode* b, size_t level, char turn){
   // When level reaches 0 or when neither player can make a move, we have to evaluate each child node
-  if(level==0||b.possibleBoard.hasWinner()||(b.possibleBoard.boardFull()&&childrenStack.empty())){
-    cout<<b.possibleBoard;
-    while(b->getParent()!=NULL){
-      cout<<b->getParent().possibleBoard;
-      b=b->getParent();
-    }
+  if(level==0||*b==nullptr){
+    evaluateUp(); //!!!!
+    return;
   }
   // Otherwise, the MiniMax function continues to generate child nodes
-  else{
-    b->generateChildren(turn);
-    level--;
+  b->generateChildren(turn);
+  level--;
+  // for each level, the minimax function will recursively call upon itself to create more children nodes
+  else if (level>1){
     for(size_t i=0; i<7;i++){
       if(children[i] == nullptr) { continue; }
       else{
         if(turn=='O') turn='X';
         else turn='O';
         MiniMax(b->getChild(i),level,turn);
+      }
+    }
+  }
+  // if level is 1, we have to generate child nodes and also their score
+  else{
+    for(size_t i=0; i<7;i++){
+      if(children[i] == nullptr) { continue; }
+      else{
+        generateScore for children; /!!!!!
       }
     }
   }
