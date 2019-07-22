8063e9dfba1d995285cebd3c3924338c40f94899
genelam26 <lamgene26@gmail.com>
2019-07-22 03:43:08 -0400

    Update treeNode.cpp

--- treeNode.cpp
+++ treeNode.cpp
@@ -7,7 +7,7 @@
 
 // default constructor ->>> ??? what else for this
 treeNode::treeNode() {
-    *parent = NULL;
+  *parent = NULL;
   new *children[7];
 }
 
@@ -16,7 +16,7 @@ treeNode::treeNode(size_t col, char user) {        // initial user input board
     Board root;
     root.addPiece(i, user);
     possibleBoard = root;
-	*parent = NULL;
+		*parent = NULL;
     new *children[7]; 
 }
 
