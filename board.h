6b711564b187e942f75a7e68b17710313375c7fa
genelam26 <lamgene26@gmail.com>
2019-07-22 14:00:48 -0400

    Update board.h

--- board.h
+++ board.h
@@ -1,11 +1,13 @@
 #ifndef __BOARD_H__
 #define __BOARD_H__
-/*
+
+#include <cstlib>
 #include <iostream>
 using std::cout;
 using std::cin;
 using std::ostream;
-*/
+
+
 class Board{
 	private:
 		static const size_t rows=6;
