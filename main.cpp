ca8fb287fd924e17bbac3e3c3c0293859a70c88e
genelam26 <lamgene26@gmail.com>
2019-07-23 14:44:13 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,29 +3,11 @@
 #include "board.h"
 #include "treeNode.h"
 
-// for when we want to make a Player vs AI Connect Four Game
-/*
-void makeMove(char Player, Board& b){
-	if(Player==init.getUser()){
-		size_t playerInput;
-		cin>>playerInput;
-		b.addPiece(Player,playerInput);
-	} 
-	else{
-		size_t AI_Input=ai.move(b);
-		b.addPiece(Player,AI_Input);
-	}
-	cout<<init;
-}
-*/
-
 int main(){
 	char user = 'O';
 	char comp = 'X';
 	
 	// Depth First Search
-	// initialize the board and stack
-	/*
 	Board initDFS; 
 	Stack<treeNode> s;
 	initDFS.addPiece(comp,0); initDFS.addPiece(user,0); initDFS.addPiece(comp,0); 
@@ -37,7 +19,6 @@ int main(){
 	treeNode dFirst(initDFS);
 	treeNode* depth=&dFirst;
 	DFS(depth,user,s);
-	*/
 	/*
 	Before               Mid		  After
 	O O - - - - -        O O O - - - -	  O O O - - - -
@@ -48,13 +29,7 @@ int main(){
 	X X X - - - -        X X X - - - -	  X X X X - - - (wow, that glo' up tho)
 	*/ 
 	
-	// Breadth First Search
-	// initialize the board again
-	//Board initBFS = new Board();
-	// test BFS
-	
-	//delete initBFS;
-	
+	/*
 	Board initMM;
 	initMM.addPiece(user,1); initMM.addPiece(user,3); initMM.addPiece(user,5);
 	initMM.addPiece(comp,2); initMM.addPiece(comp,4); initMM.addPiece(user,2);
@@ -62,6 +37,7 @@ int main(){
 	treeNode mini(initMM);
 	treeNode* max=&mini;
 	MiniMax(max,4,comp);
+	*/
 	/* Should look liek dis:
 	Before		     Mid		  Second Mid		Final
 	- - - - - - -        - - - - - - -	  - - - - - - -        - - - - - - -
@@ -72,39 +48,5 @@ int main(){
 	- O X O X O -        - O X O X O -        - O X O X O -        - O X O X O -
 	*/
 	
-	/*
-	// initalize the board again
-	Board initID = new Board();
-	// test Iterative Deepening
-	
-	delete initID;
-	*/
-	// for when we want to make a Player vs AI Connect Four Game
-	/*
-	// playerInput is the column the player chooses to put a chip into
-	while(!init.hasWinner() && !init.boardFull()){
-		makeMove(init.getUser(),init);
-		if(init.hasWinner()){
-			cout<<"You won!";
-			return;
-		}
-		makeMove(init.getComp(),init);
-		if(init.hasWinner()){
-			cout<<"You lost!";
-			return;
-		}
-	}
-	cout<<"You tied!";
-	
-	return 0;
-
-	*/
-	//char user = 'X';
-	//char comp = 'O';
-
-	//Board test=Board();
-	//test.addPiece('X', 0);
-	//treeNode bfsTest = treeNode(test);
-	//bfsTest.BFS(user);
 	return 0;
 }
