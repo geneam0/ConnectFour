c9d949436d27fd3b65b52ce5f9d6d56e86d0293a
genelam26 <lamgene26@gmail.com>
2019-07-24 16:31:16 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,30 @@ int main(){
 	char comp = 'X';
 	
 	// Depth First Search
+	/*
+	Board initDFS;
+	Stack<treeNode*> s;
+	initDFS.addPiece(comp,0); initDFS.addPiece(user,0); initDFS.addPiece(comp,0); 
+	initDFS.addPiece(user,0); initDFS.addPiece(comp,0); initDFS.addPiece(user,0);
+	initDFS.addPiece(comp,1); initDFS.addPiece(user,1); initDFS.addPiece(comp,1); 
+	initDFS.addPiece(user,1); initDFS.addPiece(comp,1); initDFS.addPiece(user,1);
+	initDFS.addPiece(comp,2); initDFS.addPiece(user,2); initDFS.addPiece(comp,2); 
+	initDFS.addPiece(user,2); initDFS.addPiece(comp,2); 
+	treeNode dFirst;							
+	dFirst.setBoard(initDFS);
+	treeNode* depth=&dFirst;
+	DFS(depth,user,s); // prints the children
+	cout<<initDFS; // prints the root
+	*/
+	/*
+	Before               Mid		  		  After
+	O O - - - - -        O O O - - - -	  	  O O O - - - -
+	X X X - - - -        X X X - - - -	 	  X X X - - - -
+	O O O - - - - _____\ O O O - - - - _____\ O O O - - - -
+	X X X - - - -      / X X X - - - -      / X X X - - - -
+	O O O - - - -        O O O - - - -	  	  O O O - - - -
+	X X X - - - -        X X X - - - -	 	  X X X X - - - 
+	*/ 
 
 	Board bfsBoard; 
 	Stack<treeNode*> s;
@@ -27,46 +51,19 @@ int main(){
 	//bfsnode.BFS(user);
 	bfsnode.IT(comp);
 
-	// DFS(depth,user,s);		
-
-
+	//MiniMax (does not work)
 	/*
-	Board initDFS;
-	Stack<treeNode*> s;
-	initDFS.addPiece(comp,0); initDFS.addPiece(user,0); initDFS.addPiece(comp,0); 
-	initDFS.addPiece(user,0); initDFS.addPiece(comp,0); initDFS.addPiece(user,0);
-	initDFS.addPiece(comp,1); initDFS.addPiece(user,1); initDFS.addPiece(comp,1); 
-	initDFS.addPiece(user,1); initDFS.addPiece(comp,1); initDFS.addPiece(user,1);
-	initDFS.addPiece(comp,2); initDFS.addPiece(user,2); initDFS.addPiece(comp,2); 
-	initDFS.addPiece(user,2); initDFS.addPiece(comp,2); 
-	treeNode dFirst;							
-	dFirst.setBoard(initDFS);
-	treeNode* depth=&dFirst;
-	DFS(depth,user,s); // prints the children
-	cout<<initDFS; // prints the root
-	*/
-	
-	// dFirst.BFS(user);
-	/*
-	Before               Mid		  		  After
-	O O - - - - -        O O O - - - -	  	  O O O - - - -
-	X X X - - - -        X X X - - - -	 	  X X X - - - -
-	O O O - - - - _____\ O O O - - - - _____\ O O O - - - -
-	X X X - - - -      / X X X - - - -      / X X X - - - -
-	O O O - - - -        O O O - - - -	  	  O O O - - - -
-	X X X - - - -        X X X - - - -	 	  X X X X - - - 
-	*/ 
-	
-	/* 
 	Board initMM;
 	initMM.addPiece(user,1); initMM.addPiece(user,3); initMM.addPiece(user,5);
 	initMM.addPiece(comp,2); initMM.addPiece(comp,4); initMM.addPiece(user,2);
 	initMM.addPiece(comp,3); initMM.addPiece(comp,4); initMM.addPiece(user,3); 
-	treeNode mini(initMM);
+	treeNode mini;
+	mini.setBoard(initMM);
 	treeNode* max=&mini;
-	MiniMax(max,4,comp);
+	MiniMax(max,2,comp);
 	*/
-	/* Should look liek dis:
+	/* 
+	Should look liek dis:
 	Before		     Mid		  Second Mid		Final
 	- - - - - - -        - - - - - - -	  - - - - - - -        - - - - - - -
 	- - - - - - -        - - - - - - -        - - - - - - -        - - - - - - -
